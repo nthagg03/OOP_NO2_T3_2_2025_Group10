194ec2a1e75ddb11deb3eed98c050940348eecfa
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-06-20 10:36:55 +0000

    update

---
+++ banHang/test/testNguoiDung.java
@@ -0,0 +1,13 @@
+package banHang.test;
+
+class testNguoiDung {
+
+
+    public static void test(){
+    Nguoidung nd = new Nguoidung();
+
+    nd.setName("Nguyen Thanh Long");
+    nd.getName();
+    }
+    
+}
