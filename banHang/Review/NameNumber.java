62c7e826fd0e58fb9a6c8b6514c649d7d3480471
Nguyễn Xuân Thắng <nguyenxtthang31@gmail.com>
2025-07-17 14:05:38 +0700

    number

---
+++ banHang/Review/NameNumber.java
@@ -0,0 +1,14 @@
+public class NameNumber {
+    private String lastName; 
+    private String telNumber; 
+    public NameNumber() {   
+    } 
+public NameNumber(String name, String num) {
+    lastName = name;
+    telNumber = num;
+    }
+    // phương thức
+    public String getLastName() { return lastName; }
+    public String getTelNumber() { return telNumber; }
+}
+
